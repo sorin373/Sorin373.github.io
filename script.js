d26308a3ee40372ee8085ea182e502bcb8cefef2
Sorin <93950240+Sorin373@users.noreply.github.com>
2023-04-03 17:47:07 +0300

    Update

--- script.js
+++ script.js
@@ -12,6 +12,6 @@ document.addEventListener('DOMContentLoaded', function () {
             slideIndex = 1
         }
         slides[slideIndex - 1].style.display = "block";
-        setTimeout(showSlides, 3000); // Change image every 5 seconds
+        setTimeout(showSlides, 3000);
     }
 }, false);
